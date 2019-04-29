e132cc36beb8c49a723def68ce8bcd2f55fc7e15
Luhao Wang <luw055@ieng6-203.ucsd.edu>
2019-04-28 17:19:25 -0700

    Header for autocomplete

--- autocomplete.cpp
+++ autocomplete.cpp
@@ -1,10 +1,20 @@
+/* Description: This is a program that stores words in a ternary tree and
+ * can autocomplete searches based on this sorted data. It can autocomplete
+ * based on a prefix, or based on a matching pattern in a word. It will 
+ * return a specified number of the most related words in the dictionary, 
+ * in alphabetical order.
+ *
+ * Name: Luhao Wang
+ * Email: luw055@ucsd.edu
+ * Date: April 28, 2019
+ * Sources: Piazza
+ */
+
 #include "DictionaryTrie.hpp"
 #include "DictionaryTrieNode.hpp"
 using namespace std;
 
 /**
- * IMPORTANT! You should use the following lines of code to match the correct output:
- * 
  * cout << "This program needs exactly one argument!" << endl;
  * cout << "Reading file: " << file << endl;
  * cout << "Enter a prefix/pattern to search for:" << endl;
